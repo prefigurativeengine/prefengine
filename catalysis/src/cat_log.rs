c12fd133495ad52882023a201e44e24dc3b39cc0
octo <octoninee@proton.me>
2024-03-17 00:00:46 -0500

    impl and test log

---
+++ catalysis/src/cat_log.rs
@@ -0,0 +1,13 @@
+use simple_logger;
+use time::macros::format_description;
+
+pub fn init_styled_logger() {
+    simple_logger::SimpleLogger::new()
+    .env()
+    .with_colors(true)
+    .with_timestamp_format(format_description!("[year]-[month]-[day] [hour]:[minute]:[second]"))
+    .init()
+    .unwrap();
+    
+}
+
--- catalysis/src/lib.rs
+++ catalysis/src/lib.rs
@@ -3,11 +3,23 @@ compile_error!("Windows only");
 
 // export app for sandbox
 pub mod app;
+pub mod cat_log;
+
+// private mod
+
+
+// use externs
+use log;
+
 
 pub trait Server {
     fn run(&self);
 }
 
 pub fn main(s: Box<dyn Server>) {
+    cat_log::init_styled_logger();
+    log::warn!("Initialized log");
+    let var = 5;
+    log::info!("Hello var: {var}");
     s.run();
 }
