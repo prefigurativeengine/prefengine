68d18cebd6bd7128e3dd6b3d6737e93a38a9a2e1
octo <octoninee@proton.me>
2024-03-16 22:05:28 -0500

    put server def in sandbox

--- catalysis/src/lib.rs
+++ catalysis/src/lib.rs
@@ -3,30 +3,11 @@ compile_error!("Windows only");
 
 // export app for sandbox
 pub mod app;
-use app::cat_engine;
 
-pub struct Server {
-    eng: cat_engine::Engine
+pub trait Server {
+    fn run(&self);
 }
 
-impl Server {
-    fn new() -> Server {
-        let e: cat_engine::Engine = cat_engine::Engine::new();
-        return Server { eng: e };
-    }
-
-    fn run(&self) {
-        self.eng.run();
-    }
-}
-
-pub fn main () {
-    let s: Box<Server> = Box::new(Server::new());
+pub fn main(s: Box<dyn Server>) {
     s.run();
 }
-
-
-
-
-
-
--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,10 +1,27 @@
 
 extern crate catalysis;
-use catalysis::app::cat_engine as cat_engine;
+use catalysis::{ app::cat_engine as cat_engine, Server };
+
+
+pub struct Sandbox {
+    eng: cat_engine::Engine
+}
+
+impl Sandbox {
+    fn new() -> Sandbox {
+        let e: cat_engine::Engine = cat_engine::Engine::new();
+        return Sandbox { eng: e };
+    }
+}
+
+impl Server for Sandbox  {
+    fn run(&self) {
+        self.eng.run();
+    }
+}
+
 
 fn main() {
-    catalysis::main();
+    let s: Box<Sandbox> = Box::new(Sandbox::new());
+    catalysis::main(s);
 }
-// pub trait Server {
-//     fn start();
-// }
