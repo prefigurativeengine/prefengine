971d587e21c113ba96c6de1b135ac1a6b828bc47
octo <octoninee@proton.me>
2024-04-09 23:51:58 -0500

    decl tcpserver

---
+++ gateways/src/ssb/mod.rs
@@ -0,0 +1,21 @@
+use tokio; // 1.37.0
+
+struct HandshakeHandler {}
+
+struct SSBTcpResponder {}
+
+struct SSBTcpServer 
+{
+    _listener: tokio::net::TcpListener,
+    _handshaker: HandshakeHandler,
+    _endpoints: HashMap<&str, &str>,
+    _client: SSBTcpResponder
+}
+
+// impl SSBServer 
+// {
+//     pub fn new()
+//     {
+
+//     }
+// }
