1b35b44f821e6c7726c8909eafe8cc6a6546a227
octo <octoninee@proton.me>
2024-04-13 02:02:20 -0500

    finish id gen

--- gateways/src/ssb/ssb_id.rs
+++ gateways/src/ssb/ssb_id.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+use std::io::BufWriter;
 use std::{error::Error, path::PathBuf};
 use std::path::Path;
 
@@ -15,11 +17,11 @@ fn get_home_dir() -> Result<String, String>
     #[cfg(not(target_os = "windows"))]
     let slash = "/";
 
-    let home_dir: PathBuf = dirs_next::home_dir().expect("User home directory should be writeable");
+    let home_dir: PathBuf = dirs_next::home_dir().expect("User home directory should be readable");
     return Ok(home_dir);
 }
 
-fn get_ind_ssb_path() -> PathBuf
+fn get_ssb_home_path() -> PathBuf
 {
     if let Some(home_dir) = get_home_dir() {
         return home_dir.push(".ssb");
@@ -27,44 +29,16 @@ fn get_ind_ssb_path() -> PathBuf
 
 }
 
-fn write_path_if_not_exist(path: &str) -> bool
-{
-    let exists: bool = Path::new(path).exists();
-    if (exists) {
-        return true;
-    } else {
-        std::fs::create_dir(path)
-            .expect("Should have been able to write the path");
-        return false;
-    }
-}
-
-fn write_to_ssbsecret(key_pair: u32) -> std::io::Result<()>
-{
-    use std::fs;
-    let mut ssb_path: PathBuf = get_ind_ssb_path();
-    // if not there, create path and write file
-    // if there, ask user if original or new one to be created, and inform of risk
-    let exists: bool = write_path_if_not_exist(&ssb_path);
-    
-    if (!exists)
-    {
-        ssb_path.push("secret");
-        std::fs::write(ssb_path, &key_pair.to_string())
-            .expect("Should have been able to read the file");
-        return Ok(());
-    }
-    else 
-    {
-        return Err(());
-    }
-}
-
 
 pub fn first_time_id_gen()
 {
     let kp_struct: OwnedIdentity = kuska_ssb::keystore::OwnedIdentity::create();
 
-    // Write and Unpin trait for writer
-    kuska_ssb::keystore::write_patchwork_config(kp_struct, writer)
+    let ssb_secret_p: PathBuf = get_ssb_home_path();
+    ssb_secret_p.push("secret");
+
+    let ssb_secret_f = File::create(ssb_secret_p).expect("Unable to create ssb secret file");
+    let ssb_secret_w: BufWriter<File> = BufWriter::new(ssb_secret_f);
+
+    kuska_ssb::keystore::write_patchwork_config(&kp_struct, ssb_secret_w);
 }
