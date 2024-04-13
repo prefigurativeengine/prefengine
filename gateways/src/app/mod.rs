604e333a38142b3161fb5dd1c28ad2c6a9b2167c
octo <octoninee@proton.me>
2024-04-13 02:03:23 -0500

    setup engine run()

--- gateways/src/app/mod.rs
+++ gateways/src/app/mod.rs
@@ -1,4 +1,3 @@
-//pub use self::cat_engine;
 use crate::local_server;
 
 pub mod cat_engine {
@@ -13,6 +12,19 @@ pub mod cat_engine {
         }
 
         pub fn run(&self) {
+            cat_log::init_styled_logger();
+            log::info!("Initialized log");
+
+            if (first_start) {
+                ssb::ssb_id::first_time_id_gen();
+            }
+
+            // auth
+
+            // sec
+
+            // circle conn
+
             crate::local_server::start();
         }
 
