6768ebb370e120832be9b58c20dc79bbe9cf22b0
octo <octoninee@proton.me>
2024-04-13 22:09:53 -0500

    begin ssb impl

--- gateways/src/ssb/mod.rs
+++ gateways/src/ssb/mod.rs
@@ -1,24 +1,108 @@
-pub mod ssb_id;
+mod ssb_id;
+mod tokio_compat_fix;
+
+use std::collections::HashMap;
+
+use kuska_ssb::{crypto::ed25519::{PublicKey, SecretKey}, keystore::OwnedIdentity};
+use kuska_sodiumoxide::crypto::{auth, sign::ed25519};
+
+use kuska_handshake::{async_std as kuska_async_std, HandshakeComplete};
+
+use async_std;
+
+// handshake_client
+use tokio::{self, 
+    net::TcpListener as TokioTcpListener, 
+    net::TcpStream as TokioTcpStream
+}; // 1.37.0
+
+use tokio_compat_fix::TokioCompatFix;
+
+// struct HandshakeHandler 
+// {
+// }
+
+// impl HandshakeHandler 
+// {
+    
+// }
 
 
-use tokio; // 1.37.0
+static _TCP_ENDPOINTS: HashMap<&str, &str> = HashMap::from([
+    ("root", "/"),
+]);
 
-struct HandshakeHandler {}
+struct SSBTcpClient 
+{
+    _streams: TokioTcpStream
+}
 
-struct SSBTcpResponder {}
+impl SSBTcpClient
+{
+    fn new() -> SSBTcpClient {  }
+}
+
+// list of peers will be used later when requests need to go to specific peers
+struct SSBPeer 
+{
+    addr: String
+}
 
 struct SSBTcpServer 
 {
-    _listener: tokio::net::TcpListener,
-    _handshaker: HandshakeHandler,
-    _endpoints: HashMap<&str, &str>,
-    _client: SSBTcpResponder
+    _listener: TokioTcpListener,
+    //_handshaker: HandshakeHandler,
+    _client: SSBTcpClient,
+    _peers: Vec<SSBPeer>
 }
 
-// impl SSBServer 
-// {
-//     pub fn new()
-//     {
+impl SSBTcpServer 
+{
+    pub fn new() -> SSBTcpServer
+    {
+        let port: &str = ":3501";
+        let public_addr: String = "0.0.0.0".to_owned() + port;
 
-//     }
-// }
+        let result: Result<TokioTcpListener, kuska_async_std::Error> = TokioTcpListener::bind(public_addr).await;
+
+    }
+
+    // TODO: make error enum for this
+    fn initiate_handshake(
+        stream: &mut TokioTcpStream,
+        net_id: auth::Key,
+        id: OwnedIdentity,
+        server_pk: ed25519::PublicKey,
+    ) -> Result<HandshakeComplete, String> 
+    {
+        let async_std_adapter: TokioCompatFix<&mut TokioTcpStream> = TokioCompatFix { 0: stream };
+
+        let client_sk: SecretKey = id.sk;
+        let client_pk: PublicKey = id.pk;
+
+        // returns custom Result type from kuska_ssb
+        let handshake_res = kuska_async_std::handshake_client(
+            &mut async_std_adapter, 
+            net_id, 
+            client_pk, 
+            client_sk, 
+            server_pk
+        ).await;
+
+        if handshake_res.is_err() {
+            return Err("Failed to perform handshake.".to_owned());
+        }
+
+        return handshake_res;
+    }
+}
+
+
+enum SSBDiscoveryMethod
+{
+    // only invitecode will be implemented for now
+    LANBroadcast,
+    InviteCode,
+    BluetoothBroadcast
+}
+
