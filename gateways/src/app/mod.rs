9b6e73f87b4011d8b3ae1167dbda0dcd70d7b321
octo <octoninee@proton.me>
2024-07-02 21:22:20 -0500

    init log first

--- gateways/src/app/mod.rs
+++ gateways/src/app/mod.rs
@@ -12,6 +12,9 @@ impl Engine
 {
     pub async fn new() -> Engine 
     {
+        cat_log::init_styled_logger();
+        log::info!("Initialized log");
+        
         let ssb_tcp_result = SSBTcpServer::new().await;
         if ssb_tcp_result.is_err() {
             panic!("Failed to init SSBTcpServer.");
@@ -26,9 +29,6 @@ impl Engine
 
     pub fn run(&self) 
     {
-        cat_log::init_styled_logger();
-        log::info!("Initialized log");
-
         let first_start = Engine::is_first_time();
         if (first_start) {
             ssb_id::first_time_id_gen();
