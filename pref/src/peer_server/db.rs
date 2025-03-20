85c40f5058ca0e8f9d2670d23494b8e13c351799
octo <octoninee@proton.me>
2025-03-20 00:06:55 -0500

    db errors

--- pref/src/peer_server/db.rs
+++ pref/src/peer_server/db.rs
@@ -18,6 +18,8 @@ const INITIATOR_CONFIRM: bool = true;
 
 use crate::core::dir;
 use crate::peer_server::Server;
+use serde_json::Error as s_Error;
+
 
 enum FailedReason {
     IncorrectVisibility,
@@ -30,18 +32,18 @@ pub fn process_local_change(changes: String, server: &mut Server) -> Result<(), 
     validate_local_chg(&changes, fail_reason);
 
     if let Err(err) = send_chg_to_overlay(&changes, server) {
-        Err(err)
+        return Err(err)
     }
     
     let overlay_confirmation_res = listen_for_confirm_fin();
     if overlay_confirmation_res.is_ok() {
-        append_chg(changes)
+        return append_chg(&changes)
     }
     overlay_confirmation_res
 }
 
 fn send_chg_to_overlay(chg: &str, server: &mut Server) -> Result<(), String> {
-    server.send_db_change(chg);
+    return server.send_db_change(chg.to_owned());
 }
 
 fn listen_for_confirm_fin() -> Result<(), String> {
@@ -54,29 +56,52 @@ fn on_confirm_fin() -> Result<(), String> {
     Ok(())
 }
 
+
 // TODO: validations and confirms not implemented yet
 pub fn process_remote_change(changes: String) -> Result<(), String> {
     // validate and checks
     
     let mut fail_reason: Option<FailedReason> = None;
-    validate_remote_chg(&changes, fail_reason);
+    validate_remote_chg(&changes, &mut fail_reason);
 
-
-    // send success or err msg of above
-    let overlay_confirmation_res: bool;
-    if INITIATOR_CONFIRM {
-        overlay_confirmation_res = initiator_confirm(&validated_map, fail_reason);
-    } else {
-        overlay_confirmation_res = peer_confirm(&validated_map, fail_reason);
+    let change_map_r: Result<HashMap<String, Value>, s_Error> = serde_json::from_str(&changes);
+    if let Err(err) = change_map_r {
+        return Err(err.to_string());
     }
 
-    // when all succeed msg recieved, push changes to disk
-    if overlay_confirmation_res {
-        write_remote_chg(validated_map);
+    let change_map: HashMap<String, Value> = change_map_r.unwrap();
+    // send success or err msg of above
+    if INITIATOR_CONFIRM {
+        let confirm_res: Result<(), String> = initiator_confirm(fail_reason);
+
+        // when all succeed msg recieved, push changes to disk
+        match confirm_res {
+            Ok(()) => {
+                write_remote_chg(&change_map);
+                Ok(())
+            },
+            Err(e) => {
+                log::error!("Overlay confirm failed: {}", e);
+                Err(e)
+            }
+        }
+    } 
+    else {
+        let confirm_res: Result<(), String> = peer_confirm(fail_reason);
+        match confirm_res {
+            Ok(v) => {
+                write_remote_chg(&change_map);
+                Ok(())
+            },
+            Err(e) => {
+                log::error!("Overlay confirm failed: {}", e);
+                Err(e)
+            }
+        }
     }
 }
 
-fn validate_remote_chg(msg: &str) {
+fn validate_remote_chg(msg: &str, fail_reason: &mut Option<FailedReason>) {
     /*
     iniatior visibility
     size check (involving peer bandwidth check)
@@ -87,7 +112,7 @@ fn validate_remote_chg(msg: &str) {
 
 }
 
-fn validate_local_chg(msg: &str) {
+fn validate_local_chg(msg: &str, fail_reason: Option<FailedReason>) {
     /*
     self visibility
     size check
@@ -98,41 +123,57 @@ fn validate_local_chg(msg: &str) {
 
 }
 
-fn initiator_confirm(msg: &str, fail_reason: Option<FailedReason>) -> Result<(), String> {
+fn initiator_confirm(fail_reason: Option<FailedReason>) -> Result<(), String> {
     Ok(())
 }
 
-fn peer_confirm(msg: &str, fail_reason: Option<FailedReason>) -> Result<(), String> {
+fn peer_confirm(fail_reason: Option<FailedReason>) -> Result<(), String> {
     Ok(())
 }
 
-
+use std::collections::HashMap;
+use serde_json::Value;
 fn write_remote_chg(msg: &HashMap<String, Value>) -> Result<(), String> {
     // append csv file
-    let data_chg: String = msg.get("data").unwrap();
-    return append_chg(data_chg);
+    if let Some(data) = msg.get("data") {
+        if let Value::String(data_s) = data {
+            return append_chg(data_s);
+        } 
+        
+        else {
+            return Err("incorrect value for id".to_owned()) 
+        }
+    }
+    return Err("incorrect msg".to_owned()) 
 }
 
-pub fn append_chg(chg: String) -> Result<(), String>  {
+pub fn append_chg(chg: &str) -> Result<(), String>  {
     // TODO: maybe change this to a command sent to a db thread
 
     let db_path_res = dir::get_root_file_path("db.csv");
     match db_path_res {
         Ok(db_path) => {
-            let db_csv_r = fs::read_to_string(db_path);
-            if db_csv_r.is_err() {
-                return db_csv_r;
+            let db_csv_r = std::fs::read_to_string(&db_path);
+            if let Err(err) = db_csv_r {
+                return Err(err.to_string());
             }
 
-            let db_csv_s: &mut String = &mut db_csv_r.unwrap();
+            let db_csv_s: String = db_csv_r.unwrap();
 
             let appended_db_csv = db_csv_s + chg;
+            
+            match std::fs::write(db_path, appended_db_csv) {
+                Ok(()) => {
+                    Ok(())
+                },
+                Err(e) => {
+                    Err(e.to_string())
+                }
+            }
 
-            fs::write(db_path, appended_db_csv);
         }
         Err((msg)) => {
-            log::error!(msg);
-            Err(("Failed to get db from disk"));
+            return Err(("Failed to get db from disk".to_owned()));
         }
     }
 }
@@ -141,16 +182,15 @@ pub fn db_to_str() -> Result<String, String> {
     let db_path_res = dir::get_root_file_path("db.csv");
     match db_path_res {
         Ok(db_path) => {
-            let db_csv_r = fs::read_to_string(db_path);
+            let db_csv_r = std::fs::read_to_string(db_path);
             if db_csv_r.is_err() {
-                db_csv_r;
+                return Err(("Failed to read csv".to_owned()));
+            } else {
+                return Ok(db_csv_r.unwrap())
             }
-
-            return Ok(db_csv_r.unwrap())
         }
         Err((msg)) => {
-            log::error!(msg);
-            Err(("Failed to get db from disk"));
+            return Err(("Failed to get db from disk".to_owned()));
         }
     }
 }
